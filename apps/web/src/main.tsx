4e07e54baddf44a6538bf6bb69716c22b0b42e67
Nurul Amin <nurul.amin.naim@gmail.com>
2026-06-27 13:27:37 +1000

    Fix trailing-slash routing for /wc2026/app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider, ThemeProvider } from './context/Providers';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider, ThemeProvider } from "./context/Providers";
+import App from "./App";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ThemeProvider>
+			<AuthProvider>
+				<BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+					<App />
+				</BrowserRouter>
+			</AuthProvider>
+		</ThemeProvider>
+	</React.StrictMode>,
 );
